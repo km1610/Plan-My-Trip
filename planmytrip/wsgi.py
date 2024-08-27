5b5badedacad408b6bdd1a1a9a50bb32a9620e0d
K. M. <90254516+km1610@users.noreply.github.com>
2024-08-27 21:12:50 +0530

    Update wsgi.py

--- planmytrip/wsgi.py
+++ planmytrip/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smarttravels.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planmytrip.settings')
 
 application = get_wsgi_application()
 
