d378ef13eba9e0776967d2a6a09761e85dbbe98d
K. M. <90254516+km1610@users.noreply.github.com>
2025-11-07 01:43:11 +0530

    Refactor functions to use new API client

--- app/functions.py
+++ app/functions.py
@@ -6,33 +6,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 def GenerateItinerary(data):
-    genai.configure(api_key=os.environ.get("GEN_AI_API"))
+    client = genai.Client(api_key=os.environ.get("GEN_AI_API"))
 
-    model = genai.GenerativeModel('gemini-pro')
-
-    response = model.generate_content([f"Generate an itinerary for {data.to_address} from {data.from_date} to {data.to_date} considering that there are {data.adults} adults and {data.children} children for the trip and budget for the trip is {data.budget} INR and travel companion will be {data.travel_companion} and specific preferences for the trip should be {data.preferences} and for special requests we have {data.special_requests}. Traveller/s will be travelling from {data.from_address}"],safety_settings=[{
-    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_NONE",
-    },{
-      "category": "HARM_CATEGORY_HATE_SPEECH",
-      "threshold": "BLOCK_NONE",
-    },{
-      "category": "HARM_CATEGORY_HARASSMENT",
-      "threshold": "BLOCK_NONE",
-    },{
-      "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-      "threshold": "BLOCK_NONE",
-    }])
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"Generate an itinerary for {data.to_address} from {data.from_date} to {data.to_date} considering that there are {data.adults} adults and {data.children} children for the trip and budget for the trip is {data.budget} INR and travel companion will be {data.travel_companion} and specific preferences for the trip should be {data.preferences} and for special requests we have {data.special_requests}. Traveller/s will be travelling from {data.from_address}",
+    )
 
     return response.text
 
 def GetCity(data):
-    genai.configure(api_key=os.environ.get("GEN_AI_API"))
+    client = genai.Client(api_key=os.environ.get("GEN_AI_API"))
 
-    model = genai.GenerativeModel('gemini-pro')
-
-    response = model.generate_content([f"If {data.to_address} is a city then just give {data.to_address} otherwise give its capital city. Just the name of the city and nothing else!Don't form any sentences!"])
-
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"If {data.to_address} is a city then just give {data.to_address} otherwise give its capital city. Just the name of the city and nothing else!Don't form any sentences!",
+    )
+    
     return response.text
 
 def FetchWeatherData(to_address, data):
@@ -43,3 +33,4 @@ def FetchWeatherData(to_address, data):
   print(response)
   print(os.environ.get("WEATHER_API_2"))
   return response.json
+
